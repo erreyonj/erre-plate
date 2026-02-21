bddac4909186eb9d350ba66b98724a86715d67b2
erreyonj <154254004+erreyonj@users.noreply.github.com>
2026-02-21 16:47:45 -0600

    add AuthController, RoleMiddleware scaffold, RouteServiceProvider adjustments

---
+++ backend/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+{
+    $this->routes(function () {
+
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(function () {
+
+                require base_path('routes/api.php');
+                require base_path('routes/auth.php');
+                require base_path('routes/customer.php');
+                require base_path('routes/admin.php');
+                require base_path('routes/chefs.php');
+                require base_path('routes/orders.php');
+            });
+
+    });
+}
+}
