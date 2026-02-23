90128c8a103d9911f8517bfe28350d1441405c58
erreyonj <154254004+erreyonj@users.noreply.github.com>
2026-02-22 21:57:00 -0600

    fixed controller namespace

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
