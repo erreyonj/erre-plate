9d9ca76e8d9b278e80a5e3e473405da822c8ab94
erreyonj <154254004+erreyonj@users.noreply.github.com>
2026-02-26 03:45:46 -0600

    frontend api base

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { type InternalAxiosRequestConfig } from 'axios';
 
-// const API_BASE = import.meta.env.VITE_API_URL;
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = import.meta.env.VITE_API_URL;
+// const API_BASE = 'http://localhost:8000/api';
 
 export const api = axios.create({
   baseURL: API_BASE,
